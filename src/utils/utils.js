b9758fd1e447d4cf6908933ca75af6bd63418774
Giulian Biolo <giulianbiolo@gmail.com>
2024-03-29 22:12:25 +0100

    Fixed bug missing function, (to check if correct behaviour)

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -29,8 +29,21 @@ function getWeekRangeInISOString() {
     return { startDate, endDate }
 }
 
+function getTodayRangeInISOString() {
+    const today = new Date().setHours(0, 0, 0, 0)
+    const startDate = new Date(today).toISOString().toLocaleString("it-IT")
+
+    const tomorrow = new Date()
+    tomorrow.setDate(tomorrow.getDate() + 1)
+    tomorrow.setHours(0, 0, 0, 0)
+    const endDate = tomorrow.toISOString().toLocaleString("it-IT")
+
+    return { startDate, endDate }
+}
+
 module.exports = {
     dateToEpoch,
     getWeekStartDatesInEpoch,
     getWeekRangeInISOString,
+    getTodayRangeInISOString,
 }
