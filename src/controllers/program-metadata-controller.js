909b2b5f285c6a33b0bcd72eb0e97c6c0e47b9ae
Daniel203 <danielarduini47@gmail.com>
2024-04-03 17:15:53 +0200

    feat: metadata details for movie/tv-show

--- src/controllers/program-metadata-controller.js
+++ src/controllers/program-metadata-controller.js
@@ -6,6 +6,10 @@ const THE_MOVIE_DB_API_KEY = process.env.THE_MOVIE_DB_API_KEY || ""
 const SEARCH_MOVIE = `${THE_MOVIE_DB_API_URL}/search/movie?query={query}&language=it-IT&page=1`
 const SEARCH_TV_SHOW = `${THE_MOVIE_DB_API_URL}/search/tv?query={query}&language=it-IT&page=1`
 
+const GET_MOVIE_DETAILS = `${THE_MOVIE_DB_API_URL}/movie/{id}?language=it-IT`
+const GET_TV_SHOW_DETAILS = `${THE_MOVIE_DB_API_URL}/tv/{id}?language=it-IT`
+
+
 const headers = {
     headers: {
         accept: "application/json",
@@ -29,6 +33,8 @@ class ProgramMetadataController {
             if (response.status == 200) {
                 req.log.info("TMDB API response is OK")
                 res.send({ data: response.data })
+            } else {
+                throw new Error("Error calling TMDB API")
             }
         } catch (error) {
             req.log.error(`Error search movies: ${error.message}`)
@@ -49,10 +55,54 @@ class ProgramMetadataController {
             if (response.status == 200) {
                 req.log.info("TMDB API response is OK")
                 res.send({ data: response.data })
+            } else {
+                throw new Error("Error calling TMDB API")
             }
         } catch (error) {
             req.log.error(`Error search tv shows: ${error.message}`)
             res.status(500).send({ error: { message: "Error search tv shows" } })
+        }
+    }
+
+    async getMovieDetails(req, res) {
+        try {
+            const id = req.params.id
+
+            var url = GET_MOVIE_DETAILS.replace("{id}", id)
+            req.log.info(`Calling TMDB API: ${url}`)
+
+            const response = await axios.get(url, headers)
+
+            if (response.status == 200) {
+                req.log.info("TMDB API response is OK")
+                res.send({ data: response.data })
+            } else {
+                throw new Error("Error calling TMDB API")
+            }
+        } catch (error) {
+            req.log.error(`Error getting movie details: ${error.message}`)
+            res.status(500).send({ error: { message: "Error getting movie details" } })
+        }
+    }
+
+    async getTvShowDetails(req, res) {
+        try {
+            const id = req.params.id
+
+            var url = GET_TV_SHOW_DETAILS.replace("{id}", id)
+            req.log.info(`Calling TMDB API: ${url}`)
+
+            const response = await axios.get(url, headers)
+
+            if (response.status == 200) {
+                req.log.info("TMDB API response is OK")
+                res.send({ data: response.data })
+            } else {
+                throw new Error("Error calling TMDB API")
+            }
+        } catch (error) {
+            req.log.error(`Error getting tv show details: ${error.message}`)
+            res.status(500).send({ error: { message: "Error getting tv show details" } })
         }
     }
 }
