1647d69e8e0bf359c965bd544d412a3669d273ac
Daniel203 <danielarduini47@gmail.com>
2024-05-16 07:28:53 +0200

    feat: api get week programs

--- src/controllers/db-controller.js
+++ src/controllers/db-controller.js
@@ -1,5 +1,5 @@
 const db = require("../managers/db-manager")
-const { getTodayRangeInISOString } = require("../utils/utils")
+const { getTodayRangeInISOString, getWeekRangeInISOString } = require("../utils/utils")
 
 class DbController {
     async getLastTvProgramUpdate(req, res) {
@@ -37,6 +37,23 @@ class DbController {
         req.log.info("Getting today's TV program from the database")
 
         const { startDate, endDate } = getTodayRangeInISOString()
+        const { data, error } = await db.rpc("tv_program_in_range_get", {
+            start_time_range: startDate,
+            end_time_range: endDate,
+        })
+
+        if (error) {
+            throw new Error(error.message)
+        }
+
+        req.log.info("Db response is OK")
+        res.send({ data: data })
+    }
+
+    async getWeekTvProgram(req, res) {
+        req.log.info("Getting week's TV program from the database")
+
+        const { startDate, endDate } = getWeekRangeInISOString()
         const { data, error } = await db.rpc("tv_program_in_range_get", {
             start_time_range: startDate,
             end_time_range: endDate,
